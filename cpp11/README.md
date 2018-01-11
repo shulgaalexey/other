8f7e71f005cc7b4ba8348bcf99cd3f42fbdf81ee
shulga <a.shulga@samsung.com>
2018-01-11 13:31:35 +0900

    Starting a folder with C++ 11 samples
    Signed-off-by: shulga <a.shulga@samsung.com>

---
+++ cpp11/README.md
@@ -0,0 +1,9 @@
+Studying C++ 11
+==============
+
+
+How to build
+------------
+
+```
+g++ -std=c++1y -Wall -g -o test hello_lambda.cpp
---
+++ cpp11/hello_lambda.cpp
@@ -0,0 +1,67 @@
+// Lambda functions in C++11
+// Lesson: https://www.youtube.com/watch?v=uk0Ytomv0wY
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+// Basic lambda features and syntax
+void lambda_demo1() {
+	int n = 42;
+	int a = 1;
+
+	auto my_lambda = [n, &a](const std::string &str) -> int {
+		std::cout << str << n << std::endl;
+		a = 13;
+		return a;
+	};
+
+	std::cout << my_lambda("Hello lambda") << std::endl;
+
+	//auto my_lambda_with_sugar = [=, &n](const std::string &str) {
+	auto my_lambda_with_sugar = [&](const std::string &str) {
+		std::cout << "lambda sugar" << std::endl;
+		n = 43;
+		a = 2;
+		return n + a;
+	};
+
+	std::cout << my_lambda_with_sugar("Lambda with sugar") << std::endl;
+
+
+}
+
+// Using lambda in iterators
+void lambda_demo2() {
+	std::vector<int> arr = {1, 2, 3, 4, 5};
+	int total = 0;
+
+	std::for_each(begin(arr), end(arr),
+			[&](int x) {
+				total += x;
+			});
+
+	std::cout << "Sum is: " << total << std::endl;
+}
+
+// Using lambda for sorting
+void lambda_demo3() {
+	std::vector<int> arr = {3, 1, 5, 4, 2};
+	std::sort(begin(arr), end(arr),
+			[](const int &a, const int &b) -> bool {
+				return a > b;
+			});
+	std::for_each(begin(arr), end(arr),
+			[](int x) {
+				std::cout << x << std::endl;
+			});
+}
+
+int main(void) {
+
+	lambda_demo1();
+	lambda_demo2();
+	lambda_demo3();
+
+	return 0;
+}
