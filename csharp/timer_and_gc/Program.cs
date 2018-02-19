e3098162de64c7de2d90abed82306ac728094115
shulga <a.shulga@samsung.com>
2018-02-19 16:24:30 +0900

    Add timer and gc investigations
    Signed-off-by: shulga <a.shulga@samsung.com>

---
+++ csharp/timer_and_gc/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace timer_and_gc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+						Timer t = new Timer(TimerCallback, null, 0, 2000);
+
+						Console.WriteLine(GC.MaxGeneration);
+
+						Console.ReadLine();
+        }
+
+				static void TimerCallback(Object o) {
+					Console.WriteLine("Timer callback" + DateTime.Now);
+
+					GC.Collect();
+				}
+    }
+}
